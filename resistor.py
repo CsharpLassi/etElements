ec825afd1cc40fa67505669f044c65f0d094c4ad
lassi <csharplassi@posteo.de>
2017-09-23 09:48:20 +0200

    include diff in resitor

--- resistor.py
+++ resistor.py
@@ -3,20 +3,24 @@
 import argparse
 
 
+class Resistor:
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser(description="etElements Resistor")
 
     parser.add_argument('-v', dest='voltage', help='Resistor Voltage', type=float)
     parser.add_argument('-r', dest='value', help='Resistor Value', type=float)
     parser.add_argument('-i', dest='current', help='Resistor Current', type=float)
-    parser.add_argument('-d', default=0.1, dest='diff', help='Validation Range', type=float)
+    parser.add_argument('-d', default=0.1, dest='diff', help='Validation Range in Percent (0.1)', type=float)
 
     result = parser.parse_args()
     if result.voltage and result.value and result.current:
         calcValue = result.voltage / result.current
-        print(abs(calcValue - result.value) < result.diff)
+        print(abs(calcValue - result.value) < result.value * result.diff)
     elif result.voltage and result.value:
-        print(result.voltage/result.value)
+        print(result.voltage / result.value)
     elif result.value and result.current:
         print(result.value * result.current)
     elif result.voltage and result.current:
@@ -26,4 +30,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main();
+    main()
